865ec6c300845ff7fd9d575875223a32bf0fc0c9
artfuldev <hello@artful.dev>
2024-09-11 17:26:28 +0100

    Add some more tests

--- node/tests/space_cli_tests.rs
+++ node/tests/space_cli_tests.rs
@@ -10,6 +10,7 @@ mod tests {
     use spaced::config::ExtendedNetwork;
     use spaced::rpc::ServerInfo;
     use std::process::Command;
+    use wallet::WalletExport;
 
     fn setup(spaced_rpc_url: String, args: &[&str]) -> Result<Command> {
         let mut space_cli = Command::cargo_bin("space-cli")?;
@@ -67,4 +68,69 @@ mod tests {
         Ok(())
     }
 
+    #[test]
+    fn create_named_wallet_works() -> Result<()> {
+        let spaced = SpaceD::new()?;
+        let mut create_named_wallet = setup(spaced.spaced_rpc_url(), &["createwallet", "custom"])?;
+
+        create_named_wallet
+            .assert()
+            .success()
+            .stdout(predicate::str::is_empty());
+
+        Ok(())
+    }
+
+    #[test]
+    fn create_duplicate_named_wallet_fails() -> Result<()> {
+        let spaced = SpaceD::new()?;
+        let name = "custom";
+        let _ = setup(spaced.spaced_rpc_url(), &["createwallet", name])?.status()?;
+        let mut create_wallet_again = setup(spaced.spaced_rpc_url(), &["createwallet", name])?;
+
+        create_wallet_again
+            .assert()
+            .success()
+            .stdout(predicate::str::contains(format!(
+                "Wallet `{}` already exists",
+                name
+            )));
+
+        Ok(())
+    }
+
+    #[test]
+    fn wallet_export_works() -> Result<()> {
+        let spaced = SpaceD::new()?;
+        let _ = setup(spaced.spaced_rpc_url(), &["createwallet"])?.status()?;
+        let mut export_wallet = setup(spaced.spaced_rpc_url(), &["exportwallet"])?;
+
+        export_wallet
+            .assert()
+            .success()
+            .stdout(predicate::function(|x: &str| {
+                let export: WalletExport = from_str(x).unwrap();
+                return export.label == "default";
+            }));
+
+        Ok(())
+    }
+
+    #[test]
+    fn named_wallet_export_works() -> Result<()> {
+        let spaced = SpaceD::new()?;
+        let name = "custom";
+        let _ = setup(spaced.spaced_rpc_url(), &["createwallet", name])?.status()?;
+        let mut export_wallet = setup(spaced.spaced_rpc_url(), &["exportwallet", name])?;
+
+        export_wallet
+            .assert()
+            .success()
+            .stdout(predicate::function(|x: &str| {
+                let export: WalletExport = from_str(x).unwrap();
+                return export.label == name;
+            }));
+
+        Ok(())
+    }
 }
